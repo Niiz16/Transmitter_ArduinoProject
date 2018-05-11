2f33cb402ae49f4a8c7b5fc84513787d632f7bdf
Niz Nuñez <niizhe16@gmail.com>
2018-05-11 12:01:32 -0500

    Add a python code to work with Arduino Serial

---
+++ arduino_serial.py
@@ -0,0 +1,29 @@
+import time
+import serial
+
+start_time = time.time()
+
+arduino = serial.Serial('COM5',115200)
+file_object = open('salida.txt', 'w')
+
+# while 1:
+#     rawString = arduino.read()
+#     file_object.write(rawString)
+#     if rawString[0] =='X':
+#         break
+
+out1 = ''
+while 1:
+    out = arduino.read(1)
+    out1 += out
+    if out == 'X':
+        break
+
+file_object.write(out1)
+file_object.close()
+arduino.close()
+
+
+elapsed_time = time.time() - start_time
+
+print 'Elapsed time %.3f' % (elapsed_time/60.0)
